e8ee73e22314c93a5b2f95f72ff18677b2d44bd0
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-05-10 13:35:17 +0530

    Finished price tracker program! Have to add README

---
+++ price_tracking.py
@@ -0,0 +1,83 @@
+import requests
+import smtplib
+import time
+import sys
+from bs4 import BeautifulSoup
+
+
+class PriceTracker:
+    def __init__(self, url, target, smail):
+        self.url = url
+        self.target = target
+        self.smail = smail
+        self.headers = {
+            "User-Agent": "GET YOUR USER-AGENT DATA BY SEARCHING MY USER AGENT ON YOUR BROWSER"
+        }
+        self.conn = smtplib.SMTP('smtp.gmail.com', 587)
+        self.conn.ehlo()
+        self.conn.starttls()
+        self.conn.login(
+            'YOUR EMAIL', 'YOUR APP PASSWORD - https://support.google.com/accounts/answer/185833?hl=en')
+        self.driver()
+
+    def driver(self):
+        while True:
+            self.check_price()
+            print("Going to check tomorrow")
+            time.sleep(24*60*60)
+
+    def check_price(self):
+        self.res = requests.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(self.res.content, 'html.parser')
+        soup.encode('utf-8')
+        self.title = soup.find(id="productTitle").get_text().strip()
+        price_elem = soup.find(id="priceblock_ourprice")
+        self.price = price_elem.get_text().replace(',', '').replace('₹', '').replace(' ', '').strip()
+        pf = float(self.price)
+        if pf <= self.target:
+            self.send_mail()
+        else:
+            return
+
+    def send_mail(self):
+        self.msg = 'Subject:Amazon Product Available!\n\nYour product: {} is now available for price: {}.\nBuy now at url: {}'.format(
+            self.title, self.price, self.url)
+        self.conn.sendmail('akushroff@gmail.com', self.smail, self.msg)
+        self.conn.quit()
+        sys.exit()
+
+
+def main():
+    print("WELCOME TO THE AMAZON PRICE TRACKER!")
+    print("-"*15)
+    print("Enter amazon url to track:")
+    while True:
+        url = input()
+        if 'amazon' not in url.split('.'):
+            print("Please enter valid amazon url:")
+            continue
+        else:
+            break
+    print("Enter target price:")
+    while True:
+        p = input()
+        pt = p.replace(',', '').replace(' ', '').strip()
+        try:
+            target = float(pt)
+            break
+        except:
+            print("Please enter valid amount without currency sign and commas:")
+            continue
+    print("Enter mail to notify:")
+    smail = input()
+    print("Now checking website once a day.")
+    try:
+        PriceTracker(url, target, smail)
+    except KeyboardInterrupt:
+        print("Exiting.")
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
+# check_price('https://www.amazon.in/dp/B01JMEDYCK/ref=psdc_1375248031_t1_B07F1NMKLM?th=1', None)
